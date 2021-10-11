673db6b7ef8454a2a0f985a31786a047a21ad7e8
BUPTfba <1172660581@qq.com>
2021-10-11 14:22:41 +0800

    hotfix first commit

--- src/main/java/com/bupt/git/gitTest.java
+++ src/main/java/com/bupt/git/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("hello git!");
         System.out.println("hello git22!");
         System.out.println("hello git33!");
+        System.out.println("hot-fix!");
     }
 }
