7fb1cff28be570c21c45115c06a8fb4cc68af9a5
BUPTfba <1172660581@qq.com>
2021-10-11 14:27:36 +0800

    master first commit

--- src/main/java/com/bupt/git/gitTest.java
+++ src/main/java/com/bupt/git/gitTest.java
@@ -6,5 +6,7 @@ public class gitTest {
         System.out.println("hello git22!");
         System.out.println("hello git33!");
         System.out.println("hot-fix!");
+        System.out.println("master commit!");
+
     }
 }
